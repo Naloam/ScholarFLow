ff7dc9191cf94eba2f20891c8cbf24088e063e6b
developer <developer@users.noreply.github.com>
2026-03-11 20:37:13 +0800

    add tutor/editor routes

--- backend/main.py
+++ backend/main.py
@@ -12,6 +12,8 @@ from api.export import router as export_router
 from api.templates import router as templates_router
 from api.tasks import router as tasks_router
 from api.chunks import router as chunks_router
+from api.tutor import router as tutor_router
+from api.editor import router as editor_router
 
 app = FastAPI(title="ScholarFlow API", version="0.1.0")
 
@@ -27,3 +29,5 @@ app.include_router(export_router)
 app.include_router(templates_router)
 app.include_router(tasks_router)
 app.include_router(chunks_router)
+app.include_router(tutor_router)
+app.include_router(editor_router)
